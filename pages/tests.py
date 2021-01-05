b68a0b0f4e868e73c3df06ebc221e5e54c51c3bc
Julie-Hrabenko <Julie-Hrabenko@users.noreply.github.com>
2021-01-05 13:07:49 +0200

    test 2

--- pages/tests.py
+++ pages/tests.py
@@ -1,10 +1,10 @@
 from django.test import SimpleTestCase
 from django.urls import reverse, resolve
 
-from .views import HomePagesView
+from .views import HomePagesView, AboutPagesView
 
 
-class HomepageTests(SimpleTestCase):
+class HomePageTests(SimpleTestCase):
     def setUp(self):
         url = reverse('home')
         self.response = self.client.get(url)
@@ -24,3 +24,25 @@ class HomepageTests(SimpleTestCase):
     def test_homepgage_url_resolves_homepagesview(self):
         view = resolve('/')
         self.assertEqual(view.func.__name__, HomePagesView.as_view().__name__)
+
+
+class AboutPageTests(SimpleTestCase):
+    def setUp(self):
+        url = reverse('about')
+        self.response = self.client.get(url)
+
+    def test_aboutpage_status_code(self):
+        self.assertEqual(self.response.status_code, 200)
+
+    def test_aboutpage_template(self):
+        self.assertTemplateUsed(self.response, 'about.html')
+
+    def test_aboutpage_contains_correct_html(self):
+        self.assertContains(self.response, 'about')
+
+    def test_aboutpage_does_not_contain_incorrect_html(self):
+        self.assertNotContains(self.response, 'Hi there! I should not be on the page.')
+
+    def test_aboutpage_url_resolves_aboutpagesview(self):
+        view = resolve('/about/')
+        self.assertEqual(view.func.__name__, AboutPagesView.as_view().__name__)
