a401f0f51ec611eda1c5f37415734d507d00d3b2
Julie-Hrabenko <Julie-Hrabenko@users.noreply.github.com>
2021-04-09 15:02:54 +0300

    Update views.py

--- articles/views.py
+++ articles/views.py
@@ -1,9 +1,7 @@
-from django.contrib.postgres.search import SearchVector
 from django.shortcuts import render, get_object_or_404
-from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from .models import Article, Category
-from .forms import CommentForm, SearchForm
-from django.views.generic import ListView, DetailView
+from .forms import CommentForm
+from django.views.generic import ListView
 from django.db.models import Q
 
 
@@ -13,42 +11,6 @@ class ArticleListView(ListView):
     paginate_by = 6
     template_name = 'articles/article_list.html'
 
-
-# def article_category(request, category_slug=None):
-#   category = None
-#  categories = Category.objects.all()
-# articles = Article.objects.filter(status='published')
-# if category_slug:
-#   category = get_object_or_404(Category, slug=category_slug)
-#  articles = articles.filter(category=category)
-
-# template = 'articles/article_detail.html'
-# context = {'articles': articles,
-#          'category': category,
-#         'categories': categories}
-
-# return render(request, template, context)
-# def article_list(request, category_slug=None):
-#   category = None
-#  categories = Category.objects.all()
-# articles = Article.objects.filter(status='published')
-# if category_slug:
-#   category = get_object_or_404(Category, slug=category_slug)
-#  articles = articles.filter(category=category)
-# page = request.GET.get('page', 1)
-# paginator = Paginator(articles, 6)
-# try:
-#   pages = paginator.page(page)
-# except PageNotAnInteger:
-#    pages = paginator.page(1)
-# except EmptyPage:
-#   pages = paginator.page(paginator.num_pages)
-# template = 'articles/article_list.html'
-# context = {'articles': articles,
-#          'category': category,
-#         'categories': categories,
-#        'pages': pages}
-# return render(request, template, context)
 
 
 def article_detail(request, year, month, day, article, category_slug=None):
@@ -93,18 +55,6 @@ def article_category(request, slug):
     return render(request, 'articles/article_list_by_category.html', {'articles': articles,
                                                                       'category': category})
 
-
-#def article_search(request):
- #   form = SearchForm()
-  #  query = None
-   # results = []
-    #if 'query' in request.GET:
-     #   form = SearchForm(request.GET)
-      #  if form.is_valid():
-       #     query = request.GET.get('q')
-        #    results = Article.objects.filter(Q(title__icontains=request.GET['query']) |
-         #                                    Q(body__icontains=request.GET['query']))
-    #return render(request, 'articles/search.html', {'form': form, 'query': query, 'results': results})
 
 class SearchResultView(ListView):
     model = Article
